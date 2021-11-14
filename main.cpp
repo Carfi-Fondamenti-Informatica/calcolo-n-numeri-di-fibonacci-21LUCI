02065f016e49c8de485a3a99df143f429b1e0fd1
21LUCI <91661502+21LUCI@users.noreply.github.com>
2021-11-14 17:14:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 
 int main() {
-    int n,i=i,b=1,a=0,c;
+    int n=0,a=1,b=1,c;
     cin>>n;
-
-    cout<<b<<endl;
-    for (int i = 0; i < n; i++) {
-        c=a+b;
-        cout<<c<<endl;
-        a=b;
-        b=c;
-
+    if(n<2){
+        cout<<"errore"<<endl;
+    }else{
+        cout<<a<<"\n"<<b<<endl;
+        for (int i=2; i<n; i++) {
+            c=a+b;
+            a=b;
+            b=c;
+            cout<<c<<endl;
         }
+    }
+    // prof non so se è giusto, a un certo punto compaiono numeri negativi
+
     return 0;
 }
